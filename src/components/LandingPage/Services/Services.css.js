4dd3ebd6095b7dc220fffe2b4d6dc9a4fb7b3fb5
Darshan Chobarkar <dchobarkar@gmail.com>
2022-02-05 17:31:52 +0530

    Services section modified

--- src/components/LandingPage/Services/Services.css.js
+++ src/components/LandingPage/Services/Services.css.js
@@ -1,3 +1,87 @@
-const ServicesCSS = { root: {} };
+const ServicesCSS = {
+  root: {
+    height: "75vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  header: {
+    padding: "2rem",
+    fontSize: "var(--mobView-2)",
+    fontFamily: "Liber_Baskerville",
+  },
+
+  serviceContainer: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  serviceList: {
+    fontSize: "var(--mobView-4)",
+    fontFamily: "Lato,sans-serif",
+  },
+
+  subHeader: {
+    padding: "1rem 0 0.5rem 0",
+    fontSize: "var(--mobView-3)",
+    fontFamily: "Liber_Baskerville",
+  },
+
+  /* Tablet view */
+  "@media only screen and (min-width: 401px) and (max-width: 960px)": {
+    root: {
+      padding: "0 60px",
+    },
+
+    header: {
+      fontSize: "var(--tabView-2)",
+    },
+
+    serviceContainer: {
+      width: "100%",
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+
+    serviceList: {
+      fontSize: "var(--tabView-4)",
+    },
+
+    subHeader: {
+      padding: "2rem 0 1rem 0",
+      fontSize: "var(--tabView-3)",
+    },
+  },
+
+  /* Desktop view */
+  "@media only screen and (min-width: 961px)": {
+    root: {
+      padding: "0px 120px",
+    },
+
+    header: {
+      fontSize: "var(--desView-2)",
+    },
+
+    serviceContainer: {
+      width: "100%",
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+
+    serviceList: {
+      fontSize: "var(--desView-4)",
+    },
+
+    subHeader: {
+      padding: "2rem 0 1rem 0",
+      fontSize: "var(--desView-3)",
+    },
+  },
+};
 
 export default ServicesCSS;
--- src/components/LandingPage/Services/Services.js
+++ src/components/LandingPage/Services/Services.js
@@ -10,7 +10,57 @@ const useStyles = makeStyles({ ...ServicesCSS });
 function Services() {
   const classes = useStyles();
 
-  return <div className={classes.root}>Services</div>;
+  return (
+    <div className={classes.root}>
+      <h2 className={classes.header}>Services</h2>
+
+      <div className={classes.serviceContainer}>
+        <div className={classes.serviceList}>
+          <h3 className={classes.subHeader}>Service 1</h3>
+
+          <ul>
+            <li>Sub - service 1</li>
+            <li>Sub - service 2</li>
+            <li>Sub - service 3</li>
+            <li>Sub - service 4</li>
+          </ul>
+        </div>
+
+        <div className={classes.serviceList}>
+          <h3 className={classes.subHeader}>Service 2</h3>
+
+          <ul>
+            <li>Sub - service 1</li>
+            <li>Sub - service 2</li>
+            <li>Sub - service 3</li>
+            <li>Sub - service 4</li>
+          </ul>
+        </div>
+
+        <div className={classes.serviceList}>
+          <h3 className={classes.subHeader}>Service 3</h3>
+
+          <ul>
+            <li>Sub - service 1</li>
+            <li>Sub - service 2</li>
+            <li>Sub - service 3</li>
+            <li>Sub - service 4</li>
+          </ul>
+        </div>
+
+        <div className={classes.serviceList}>
+          <h3 className={classes.subHeader}>Service 4</h3>
+
+          <ul>
+            <li>Sub - service 1</li>
+            <li>Sub - service 2</li>
+            <li>Sub - service 3</li>
+            <li>Sub - service 4</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Services;
