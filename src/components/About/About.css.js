90385c4683894d688e612e7864a10d0e7e403117
Darshan Chobarkar <dchobarkar@gmail.com>
2022-02-08 22:31:43 +0530

    About page modified

--- src/components/About/About.css.js
+++ src/components/About/About.css.js
@@ -1,3 +1,129 @@
-const AboutCSS = { root: {} };
+const AboutCSS = {
+  root: {
+    padding: "1rem 30px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  image: {
+    display: "none",
+  },
+
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-start",
+  },
+
+  header: {
+    padding: "1rem 0",
+    color: "var(--accent)",
+    fontSize: "var(--mobView-3)",
+    fontFamily: "Libre_Baskerville",
+  },
+
+  text: {
+    paddingBottom: "2rem",
+    fontSize: "var(--mobView-4)",
+    fontFamily: "Lato,sans-serif",
+  },
+
+  mapBox: {
+    padding: "1rem 30px",
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  information: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    fontSize: "var(--mobView-4)",
+    fontFamily: "Lato,sans-serif",
+  },
+
+  map: {
+    width: "100%",
+  },
+
+  /* Tablet view */
+  "@media only screen and (min-width: 401px) and (max-width: 960px)": {
+    root: {
+      padding: "1rem 60px",
+    },
+
+    image: {
+      width: "30%",
+      display: "block",
+    },
+
+    container: {
+      width: "70%",
+    },
+
+    header: {
+      fontSize: "var(--tabView-3)",
+    },
+
+    text: {
+      fontSize: "var(--tabView-4)",
+    },
+
+    mapBox: {
+      padding: "1rem 60px",
+    },
+
+    information: {
+      fontSize: "var(--tabView-4)",
+    },
+
+    map: {
+      width: "100%",
+    },
+  },
+
+  /* Desktop view */
+  "@media only screen and (min-width: 961px)": {
+    root: {
+      padding: "1rem 120px",
+    },
+
+    image: {
+      width: "30%",
+      display: "block",
+    },
+
+    container: {
+      width: "70%",
+      padding: "0 10%",
+    },
+
+    header: {
+      fontSize: "var(--desView-3)",
+    },
+
+    text: {
+      fontSize: "var(--desView-4)",
+    },
+
+    mapBox: {
+      padding: "1rem 120px",
+    },
+
+    information: {
+      fontSize: "var(--desView-4)",
+    },
+
+    map: {
+      width: "100%",
+    },
+  },
+};
 
 export default AboutCSS;
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,72 @@ const useStyles = makeStyles({ ...AboutCSS });
 function About() {
   const classes = useStyles();
 
-  return <div className={classes.root}>About</div>;
+  return (
+    <React.Fragment>
+      <div className={classes.root}>
+        <div className={classes.image}>
+          <img
+            alt="Darshan"
+            src="https://media-exp1.licdn.com/dms/image/C4E03AQGtWm6DYqL8dg/profile-displayphoto-shrink_100_100/0/1608177914950?e=1649894400&v=beta&t=K5D8UVR12aZTjWBd21QFcXIzr1Vh3elV3XNAJEjEodw"
+          />
+        </div>
+
+        <div className={classes.container}>
+          <h2 className={classes.header}>Value 1</h2>
+
+          <p className={classes.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est nisi,
+            justo, suscipit condimentum tellus tristique massa risus.
+            Condimentum maecenas in nibh felis quis sit augue consectetur
+            pulvinar. Dui nibh nibh sit et elementum. Pretium consequat,
+            pharetra urna nisl tristique aliquet. Suspendisse congue ornare elit
+            adipiscing sed justo. Posuere nullam tortor sed rutrum. At proin
+            suspendisse at quis nulla. Sit et nulla hendrerit facilisis risus
+            suscipit. Adipiscing nunc cras nibh velit aliquam arcu ut. Dolor,
+            sed amet id accumsan tellus lobortis non aliquam pretium. Tellus,
+            laoreet proin elit velit volutpat porttitor arcu nisl. Aliquet ut
+            quam nunc arcu maecenas. Facilisis sem lacus duis sed nisl sem nisl
+            at nunc. Pretium tristique quisque a dui commodo tempor eget.
+          </p>
+
+          <h2 className={classes.header}>Value 1</h2>
+
+          <p className={classes.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est nisi,
+            justo, suscipit condimentum tellus tristique massa risus.
+            Condimentum maecenas in nibh felis quis sit augue consectetur
+            pulvinar. Dui nibh nibh sit et elementum. Pretium consequat,
+            pharetra urna nisl tristique aliquet. Suspendisse congue ornare elit
+            adipiscing sed justo. Posuere nullam tortor sed rutrum. At proin
+            suspendisse at quis nulla. Sit et nulla hendrerit facilisis risus
+            suscipit. Adipiscing nunc cras nibh velit aliquam arcu ut. Dolor,
+            sed amet id accumsan tellus lobortis non aliquam pretium. Tellus,
+            laoreet proin elit velit volutpat porttitor arcu nisl. Aliquet ut
+            quam nunc arcu maecenas. Facilisis sem lacus duis sed nisl sem nisl
+            at nunc. Pretium tristique quisque a dui commodo tempor eget.
+          </p>
+        </div>
+      </div>
+
+      <div className={classes.mapBox}>
+        <div className={classes.information}>
+          <p>Mobile No. : 9404168827</p>
+          <p>Email : dchobarkar@gmail.com</p>
+          <p>
+            Address : "Sudarshan", Dindayal Colony, <br />
+            Mandva Road,
+            <br />
+            Ambajogai 431517
+          </p>
+
+          <p>LinkedIn</p>
+          <p>Email</p>
+        </div>
+
+        <div className={classes.map}></div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default About;
