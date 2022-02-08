2cc9136661fa0ea88145c5fd56924ce023b26ae3
Darshan Chobarkar <dchobarkar@gmail.com>
2022-02-08 22:10:46 +0530

    BlogList page modified

--- src/components/BlogList/BlogList.css.js
+++ src/components/BlogList/BlogList.css.js
@@ -1,3 +1,102 @@
-const BlogListCSS = { root: {} };
+const BlogListCSS = {
+  root: {
+    minHeight: "90vh",
+    padding: "1rem 30px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  container: {
+    padding: "1rem 0",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    borderBottom: "1px solid var(--text-black)",
+  },
+
+  header: {
+    fontSize: "var(--mobView-1)",
+    fontFamily: "Libre_Baskerville",
+  },
+
+  info: {
+    padding: "0.5rem 0",
+    fontSize: "var(--mobView-4)",
+    fontFamily: "Montserrat,sans-serif",
+    fontWeight: "300",
+  },
+
+  text: {
+    fontSize: "var(--mobView-4)",
+    fontFamily: "Lato,sans-serif",
+  },
+
+  link: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "flex-end",
+    textDecoration: "none",
+    color: "var(--accent)",
+    fontSize: "var(--mobView-3)",
+    fontFamily: "Montserrat,sans-serif",
+  },
+
+  /* Tablet view */
+  "@media only screen and (min-width: 401px) and (max-width: 960px)": {
+    root: {
+      padding: "1rem 60px",
+    },
+
+    container: {
+      padding: "1rem 10%",
+    },
+
+    header: {
+      fontSize: "var(--tabView-1)",
+    },
+
+    info: {
+      fontSize: "var(--tabView-4)",
+    },
+
+    text: {
+      fontSize: "var(--tabView-4)",
+    },
+
+    link: {
+      fontSize: "var(--tabView-3)",
+    },
+  },
+
+  /* Desktop view */
+  "@media only screen and (min-width: 961px)": {
+    root: {
+      padding: "1rem 120px",
+    },
+
+    container: {
+      padding: "1rem 15%",
+    },
+
+    header: {
+      fontSize: "var(--desView-1)",
+    },
+
+    info: {
+      fontSize: "var(--desView-4)",
+    },
+
+    text: {
+      fontSize: "var(--desView-4)",
+    },
+
+    link: {
+      fontSize: "var(--desView-3)",
+    },
+  },
+};
 
 export default BlogListCSS;
--- src/components/BlogList/BlogList.js
+++ src/components/BlogList/BlogList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
+import { Link } from "react-router-dom";
 
 import BlogListCSS from "./BlogList.css";
 
@@ -10,7 +11,89 @@ const useStyles = makeStyles({ ...BlogListCSS });
 function BlogList() {
   const classes = useStyles();
 
-  return <div className={classes.root}>BlogList</div>;
+  return (
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <h2 className={classes.header}>Header</h2>
+
+        <div className={classes.info}>
+          <p>by Darshan Chobarkar | 20 Sept. 2021</p>
+        </div>
+
+        <p className={classes.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc gravida
+          a eget lobortis ullamcorper consectetur pellentesque. Potenti laoreet
+          volutpat adipiscing accumsan. Massa tellus suspendisse magnis
+          ultricies risus a dui parturient. Orci consectetur in libero enim,
+          proin donec placerat ut. Id orci vitae elementum id ut. Praesent in
+          elit viverra porttitor odio ornare consectetur egestas quis. Morbi.
+        </p>
+        <Link className={classes.link} to="/blog">
+          Read More
+        </Link>
+      </div>
+
+      <div className={classes.container}>
+        <h2 className={classes.header}>Header</h2>
+
+        <div className={classes.info}>
+          <p>by Darshan Chobarkar | 20 Sept. 2021</p>
+        </div>
+
+        <p className={classes.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc gravida
+          a eget lobortis ullamcorper consectetur pellentesque. Potenti laoreet
+          volutpat adipiscing accumsan. Massa tellus suspendisse magnis
+          ultricies risus a dui parturient. Orci consectetur in libero enim,
+          proin donec placerat ut. Id orci vitae elementum id ut. Praesent in
+          elit viverra porttitor odio ornare consectetur egestas quis. Morbi.
+        </p>
+        <Link className={classes.link} to="/blog">
+          Read More
+        </Link>
+      </div>
+
+      <div className={classes.container}>
+        <h2 className={classes.header}>Header</h2>
+
+        <div className={classes.info}>
+          <p>by Darshan Chobarkar | 20 Sept. 2021</p>
+        </div>
+
+        <p className={classes.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc gravida
+          a eget lobortis ullamcorper consectetur pellentesque. Potenti laoreet
+          volutpat adipiscing accumsan. Massa tellus suspendisse magnis
+          ultricies risus a dui parturient. Orci consectetur in libero enim,
+          proin donec placerat ut. Id orci vitae elementum id ut. Praesent in
+          elit viverra porttitor odio ornare consectetur egestas quis. Morbi.
+        </p>
+        <Link className={classes.link} to="/blog">
+          Read More
+        </Link>
+      </div>
+      <div className={classes.container}>
+        <h2 className={classes.header}>Header</h2>
+
+        <div className={classes.info}>
+          <p>by Darshan Chobarkar | 20 Sept. 2021</p>
+        </div>
+
+        <p className={classes.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc gravida
+          a eget lobortis ullamcorper consectetur pellentesque. Potenti laoreet
+          volutpat adipiscing accumsan. Massa tellus suspendisse magnis
+          ultricies risus a dui parturient. Orci consectetur in libero enim,
+          proin donec placerat ut. Id orci vitae elementum id ut. Praesent in
+          elit viverra porttitor odio ornare consectetur egestas quis. Morbi.
+        </p>
+
+        <div className={classes.link}>
+          <Link to="/blog">Read More</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BlogList;
