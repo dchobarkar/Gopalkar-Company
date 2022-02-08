399d96852effd6862c4c82001ae7b4460b52bd44
Darshan Chobarkar <dchobarkar@gmail.com>
2022-02-08 17:57:42 +0530

    Testinomial section modified

--- src/components/LandingPage/Testinomials/Testinomials.css.js
+++ src/components/LandingPage/Testinomials/Testinomials.css.js
@@ -1,3 +1,116 @@
-const TestinomialsCSS = { root: {} };
+const TestinomialsCSS = {
+  root: {
+    minHeight: "75vh",
+    padding: "1rem 30px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "var(--text-white)",
+    backgroundColor: "var(--secondary)",
+  },
+
+  header: {
+    padding: "2rem",
+    fontSize: "var(--mobView-2)",
+    fontFamily: "Liber_Baskerville",
+  },
+
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  card: {
+    paddingBottom: "1rem",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start",
+  },
+
+  image: {},
+
+  testinomial: {
+    padding: "0 1rem 1rem 1rem",
+  },
+
+  text: {
+    paddingBottom: "0.5rem",
+    fontSize: "var(--mobView-4)",
+    fontFamily: "Lato,sans-serif",
+  },
+
+  name: {
+    textAlign: "right",
+    fontSize: "var(--mobView-4)",
+    fontFamily: "Montserrat,sans-serif",
+  },
+
+  /* Tablet view */
+  "@media only screen and (min-width: 401px) and (max-width: 960px)": {
+    root: {
+      padding: "0 60px",
+    },
+    header: {
+      fontSize: "var(--tabView-2)",
+    },
+
+    container: {
+      padding: "1rem",
+    },
+
+    card: {
+      maxWidth: "350px",
+      padding: "1rem",
+    },
+
+    image: {},
+
+    testinomial: {},
+
+    text: {
+      fontSize: "var(--tabView-4)",
+    },
+
+    name: {
+      fontSize: "var(--tabView-4)",
+    },
+  },
+
+  /* Desktop view */
+  "@media only screen and (min-width: 961px)": {
+    root: {
+      padding: "0px 120px",
+    },
+    header: {
+      fontSize: "var(--desView-2)",
+    },
+
+    container: {
+      width: "100%",
+      padding: "1rem",
+      justifyContent: "space-between",
+    },
+
+    card: {
+      maxWidth: "350px",
+      padding: "1rem",
+    },
+
+    image: {},
+
+    testinomial: {},
+
+    text: {
+      fontSize: "var(--desView-4)",
+    },
+
+    name: {
+      fontSize: "var(--desView-4)",
+    },
+  },
+};
 
 export default TestinomialsCSS;
--- src/components/LandingPage/Testinomials/Testinomials.js
+++ src/components/LandingPage/Testinomials/Testinomials.js
@@ -10,7 +10,64 @@ const useStyles = makeStyles({ ...TestinomialsCSS });
 function Testinomials() {
   const classes = useStyles();
 
-  return <div className={classes.root}>Testinomials</div>;
+  return (
+    <div className={classes.root}>
+      <h2 className={classes.header}>Testimonials</h2>
+
+      <div className={classes.container}>
+        <div className={classes.card}>
+          <div className={classes.image}>image</div>
+          <div className={classes.testinomial}>
+            <p className={classes.text}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
+              facilisi rutrum dignissim eget id eget diam accumsan viverra.
+              Mauris nulla pretium cursus sit placerat curabitur est. Ultrices
+              ornare porttitor duis sed. Sem nunc vitae integer sociis sit
+              pretium nisl nisl pulvinar. Scelerisque arcu curabitur ac,
+              eleifend. Amet vitae enim, neque pharetra, eu dolor. Aliquam non
+              habitant lobortis neque sed suspendisse. Enim vehicula sem orci
+              sem turpis dui enim.
+            </p>
+            <p className={classes.name}>- User Name, Post</p>
+          </div>
+        </div>
+
+        <div className={classes.card}>
+          <div className={classes.image}>image</div>
+          <div className={classes.testinomial}>
+            <p className={classes.text}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
+              facilisi rutrum dignissim eget id eget diam accumsan viverra.
+              Mauris nulla pretium cursus sit placerat curabitur est. Ultrices
+              ornare porttitor duis sed. Sem nunc vitae integer sociis sit
+              pretium nisl nisl pulvinar. Scelerisque arcu curabitur ac,
+              eleifend. Amet vitae enim, neque pharetra, eu dolor. Aliquam non
+              habitant lobortis neque sed suspendisse. Enim vehicula sem orci
+              sem turpis dui enim.
+            </p>
+            <p className={classes.name}>- User Name, Post</p>
+          </div>
+        </div>
+
+        <div className={classes.card}>
+          <div className={classes.image}>image</div>
+          <div className={classes.testinomial}>
+            <p className={classes.text}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
+              facilisi rutrum dignissim eget id eget diam accumsan viverra.
+              Mauris nulla pretium cursus sit placerat curabitur est. Ultrices
+              ornare porttitor duis sed. Sem nunc vitae integer sociis sit
+              pretium nisl nisl pulvinar. Scelerisque arcu curabitur ac,
+              eleifend. Amet vitae enim, neque pharetra, eu dolor. Aliquam non
+              habitant lobortis neque sed suspendisse. Enim vehicula sem orci
+              sem turpis dui enim.
+            </p>
+            <p className={classes.name}>- User Name, Post</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Testinomials;
