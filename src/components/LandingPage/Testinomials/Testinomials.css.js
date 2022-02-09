16c3691561b258afbbab742adb0bba7423a77869
Darshan Chobarkar <dchobarkar@gmail.com>
2022-02-09 19:37:43 +0530

    Testinomial section completed

--- src/components/LandingPage/Testinomials/Testinomials.css.js
+++ src/components/LandingPage/Testinomials/Testinomials.css.js
@@ -1,7 +1,11 @@
+import { Lib32, Mont16, Padding, SectionHeight } from "../../../css";
+
 const TestinomialsCSS = {
   root: {
-    minHeight: "75vh",
-    padding: "1rem 30px",
+    ...SectionHeight,
+    ...Padding,
+    paddingTop: "2rem",
+    paddingBottom: "2rem",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
@@ -9,106 +13,59 @@ const TestinomialsCSS = {
     color: "var(--text-white)",
     backgroundColor: "var(--secondary)",
   },
-
-  header: {
-    padding: "2rem",
-    fontSize: "var(--mobView-2)",
-    fontFamily: "Liber_Baskerville",
-  },
-
+  header: { ...Lib32 },
   container: {
+    paddingTop: "2rem",
     display: "flex",
     flexWrap: "wrap",
     justifyContent: "center",
     alignItems: "center",
   },
-
   card: {
     paddingBottom: "1rem",
     display: "flex",
-    justifyContent: "center",
-    alignItems: "flex-start",
   },
-
-  image: {},
-
+  image: {
+    width: "3rem",
+    height: "3rem",
+  },
   testinomial: {
-    padding: "0 1rem 1rem 1rem",
+    padding: "0 0 1rem 1rem",
   },
-
   text: {
     paddingBottom: "0.5rem",
-    fontSize: "var(--mobView-4)",
-    fontFamily: "Lato,sans-serif",
   },
-
   name: {
     textAlign: "right",
-    fontSize: "var(--mobView-4)",
-    fontFamily: "Montserrat,sans-serif",
+    ...Mont16,
+    color: "var(--text-gray)",
   },
 
   /* Tablet view */
   "@media only screen and (min-width: 401px) and (max-width: 960px)": {
     root: {
-      padding: "0 60px",
+      paddingTop: "2rem",
+      paddingBottom: "2rem",
     },
-    header: {
-      fontSize: "var(--tabView-2)",
-    },
-
-    container: {
-      padding: "1rem",
-    },
-
     card: {
       maxWidth: "350px",
       padding: "1rem",
-    },
-
-    image: {},
-
-    testinomial: {},
-
-    text: {
-      fontSize: "var(--tabView-4)",
-    },
-
-    name: {
-      fontSize: "var(--tabView-4)",
     },
   },
 
   /* Desktop view */
   "@media only screen and (min-width: 961px)": {
     root: {
-      padding: "0px 120px",
+      paddingTop: "2rem",
+      paddingBottom: "2rem",
     },
-    header: {
-      fontSize: "var(--desView-2)",
-    },
-
     container: {
       width: "100%",
-      padding: "1rem",
       justifyContent: "space-between",
     },
-
     card: {
       maxWidth: "350px",
       padding: "1rem",
-    },
-
-    image: {},
-
-    testinomial: {},
-
-    text: {
-      fontSize: "var(--desView-4)",
-    },
-
-    name: {
-      fontSize: "var(--desView-4)",
     },
   },
 };
--- src/components/LandingPage/Testinomials/Testinomials.js
+++ src/components/LandingPage/Testinomials/Testinomials.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
 
+import Image1 from "../../../assets/img/gopalkar-and-company-testinomial1.png";
+import Image2 from "../../../assets/img/gopalkar-and-company-testinomial2.png";
+import Image3 from "../../../assets/img/gopalkar-and-company-testinomial3.png";
 import TestinomialsCSS from "./Testinomials.css";
 
 // CSS
@@ -11,12 +14,17 @@ function Testinomials() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <section className={classes.root}>
       <h2 className={classes.header}>Testimonials</h2>
 
       <div className={classes.container}>
         <div className={classes.card}>
-          <div className={classes.image}>image</div>
+          <img
+            className={classes.image}
+            alt="gopalkar-and-company-testinomial1"
+            src={Image1}
+          />
+
           <div className={classes.testinomial}>
             <p className={classes.text}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
@@ -28,12 +36,18 @@ function Testinomials() {
               habitant lobortis neque sed suspendisse. Enim vehicula sem orci
               sem turpis dui enim.
             </p>
+
             <p className={classes.name}>- User Name, Post</p>
           </div>
         </div>
 
         <div className={classes.card}>
-          <div className={classes.image}>image</div>
+          <img
+            className={classes.image}
+            alt="gopalkar-and-company-testinomial2"
+            src={Image2}
+          />
+
           <div className={classes.testinomial}>
             <p className={classes.text}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
@@ -45,12 +59,18 @@ function Testinomials() {
               habitant lobortis neque sed suspendisse. Enim vehicula sem orci
               sem turpis dui enim.
             </p>
+
             <p className={classes.name}>- User Name, Post</p>
           </div>
         </div>
 
         <div className={classes.card}>
-          <div className={classes.image}>image</div>
+          <img
+            className={classes.image}
+            alt="gopalkar-and-company-testinomial3"
+            src={Image3}
+          />
+
           <div className={classes.testinomial}>
             <p className={classes.text}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
@@ -62,11 +82,12 @@ function Testinomials() {
               habitant lobortis neque sed suspendisse. Enim vehicula sem orci
               sem turpis dui enim.
             </p>
+
             <p className={classes.name}>- User Name, Post</p>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
