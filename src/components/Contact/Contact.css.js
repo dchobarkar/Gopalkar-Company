a246012060595c1576fae1f054099db7eed309c2
Darshan Chobarkar <dchobarkar@gmail.com>
2022-02-09 15:13:56 +0530

    Contact page modified

--- src/components/Contact/Contact.css.js
+++ src/components/Contact/Contact.css.js
@@ -1,3 +1,64 @@
-const ContactCSS = { root: {} };
+const ContactCSS = {
+  root: {},
+  mapBox: {
+    padding: "1rem 30px",
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  information: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    fontSize: "var(--mobView-4)",
+    fontFamily: "Lato,sans-serif",
+  },
+
+  map: {
+    width: "100%",
+  },
+
+  /* Tablet view */
+  "@media only screen and (min-width: 401px) and (max-width: 960px)": {
+    root: {
+      padding: "1rem 60px",
+    },
+
+    mapBox: {
+      padding: "1rem 60px",
+    },
+
+    information: {
+      fontSize: "var(--tabView-4)",
+    },
+
+    map: {
+      width: "100%",
+    },
+  },
+
+  /* Desktop view */
+  "@media only screen and (min-width: 961px)": {
+    root: {
+      padding: "1rem 120px",
+    },
+
+    mapBox: {
+      padding: "1rem 120px",
+    },
+
+    information: {
+      fontSize: "var(--desView-4)",
+    },
+
+    map: {
+      width: "100%",
+    },
+  },
+};
 
 export default ContactCSS;
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -10,7 +10,58 @@ const useStyles = makeStyles({ ...ContactCSS });
 function Contact() {
   const classes = useStyles();
 
-  return <div className={classes.root}>Contact</div>;
+  return (
+    <div className={classes.root}>
+      <div>
+        <h2>Some Cool Heading</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Adipiscing a,
+          eleifend consequat nunc et ullamcorper luctus nisi quis. Platea ac
+          egestas integer ipsum eget tempus commodo. Sapien, vitae integer
+          porttitor elit tempus. Urna ut vulputate morbi proin consequat.
+          Maecenas risus orci lectus sit lorem id. Sit in commodo natoque eu.
+          Viverra sit imperdiet vulputate suspendisse nec gravida tempor id dui.
+          Hendrerit imperdiet dui adipiscing tellus proin ullamcorper leo a. Sed
+          volutpat morbi nullam lectus. Bibendum feugiat ipsum tincidunt sed
+          amet, ridiculus nec. Dui, nunc nibh elit elementum tristique pulvinar
+          felis, lacus. Gravida cursus amet vitae felis habitant adipiscing
+          montes, nulla. In bibendum vestibulum in morbi id. Tristique et tortor
+          fermentum aliquet adipiscing.
+        </p>
+      </div>
+
+      <form>
+        <label for="name">Name :</label>
+        <input id="name" type="text" name="name" />
+
+        <label for="email">Email :</label>
+        <input id="email" type="email" name="email" />
+
+        <label for="message">Message :</label>
+        <input id="message" type="text" name="message" />
+
+        <button type="submit">Send</button>
+      </form>
+
+      <div className={classes.mapBox}>
+        <div className={classes.information}>
+          <p>Mobile No. : 9404168827</p>
+          <p>Email : dchobarkar@gmail.com</p>
+          <p>
+            Address : "Sudarshan", Dindayal Colony, <br />
+            Mandva Road,
+            <br />
+            Ambajogai 431517
+          </p>
+
+          <p>LinkedIn</p>
+          <p>Email</p>
+        </div>
+
+        <div className={classes.map}></div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
