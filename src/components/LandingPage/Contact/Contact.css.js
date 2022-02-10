5aa91a6e31fa5ad7dfba2958ef615bfe4aa48ce0
Darshan Chobarkar <dchobarkar@gmail.com>
2022-02-10 16:45:07 +0530

    Contact section completed

--- src/components/LandingPage/Contact/Contact.css.js
+++ src/components/LandingPage/Contact/Contact.css.js
@@ -1,38 +1,97 @@
+import { Lib32, Padding, SectionHeight } from "../../../css";
+
 const ContactCSS = {
   root: {
-    minHeight: "75vh",
-    padding: "1rem 30px",
+    ...SectionHeight,
+    ...Padding,
+    paddingTop: "2rem",
+    paddingBottom: "2rem",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
   },
-
-  header: {
-    padding: "2rem",
-    fontSize: "var(--mobView-2)",
-    fontFamily: "Liber_Baskerville",
-  },
-
+  header: { ...Lib32 },
   container: {
+    paddingTop: "1rem",
     display: "flex",
-    flexWrap: "wrap",
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
   },
-
-  information: {
-    width: "100%",
+  address: {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "flex-start",
-    fontSize: "var(--mobView-4)",
-    fontFamily: "Lato,sans-serif",
+    "& div": {
+      padding: "0.25rem 0",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      "& div": {
+        marginBottom: "auto",
+        padding: "0",
+        height: "100%",
+      },
+    },
+  },
+  image: {
+    height: "1.5rem",
+    width: "1.5rem",
+  },
+  text: {
+    paddingLeft: "0.5rem",
+    lineHeight: "1.3rem",
+  },
+  mediaIcons: {
+    padding: "1rem 0",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+
+    "& img": {
+      height: "2rem",
+      width: "2rem",
+    },
+  },
+  map: {
+    height: "100%",
+    width: "100%",
   },
 
-  map: {
-    width: "100%",
+  /* Tablet view */
+  "@media only screen and (min-width: 401px) and (max-width: 960px)": {
+    root: {
+      paddingTop: "2rem",
+      paddingBottom: "2rem",
+    },
+    container: {
+      paddingTop: "2rem",
+      height: "50vh",
+      width: "100%",
+      flexDirection: "row",
+    },
+    address: {
+      minWidth: "40vw",
+    },
+  },
+
+  /* Desktop view */
+  "@media only screen and (min-width: 961px)": {
+    root: {
+      paddingTop: "2rem",
+      paddingBottom: "2rem",
+    },
+    container: {
+      paddingTop: "2rem",
+      height: "50vh",
+      width: "60vw",
+      flexDirection: "row",
+    },
+    address: {
+      minWidth: "300px",
+    },
   },
 };
 
--- src/components/LandingPage/Contact/Contact.js
+++ src/components/LandingPage/Contact/Contact.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
 
+import Icon1 from "../../../assets/img/icon-mobile.png";
+import Icon2 from "../../../assets/img/icon-email.png";
+import Icon3 from "../../../assets/img/icon-address.png";
+import Icon4 from "../../../assets/img/icon-linkedin.png";
+import Icon5 from "../../../assets/img/icon-gmail.png";
 import ContactCSS from "./Contact.css";
 
 // CSS
@@ -11,27 +16,81 @@ function Contact() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <section className={classes.root}>
       <h2 className={classes.header}>Contact</h2>
 
       <div className={classes.container}>
-        <div className={classes.information}>
-          <p>Mobile No. : 9404168827</p>
-          <p>Email : dchobarkar@gmail.com</p>
-          <p>
-            Address : "Sudarshan", Dindayal Colony, <br />
-            Mandva Road,
-            <br />
-            Ambajogai 431517
-          </p>
+        <div>
+          <div className={classes.address}>
+            <div>
+              <img
+                className={classes.image}
+                alt="gopalkar-and-company-mobile"
+                src={Icon1}
+              />
 
-          <p>LinkedIn</p>
-          <p>Email</p>
+              <p className={classes.text}>9421061989</p>
+            </div>
+
+            <div>
+              <img
+                className={classes.image}
+                alt="gopalkar-and-company-email"
+                src={Icon2}
+              />
+
+              <p className={classes.text}>gopalkar.ca@gmail.com</p>
+            </div>
+
+            <div>
+              <div>
+                <img
+                  className={classes.image}
+                  alt="gopalkar-and-company-address"
+                  src={Icon3}
+                />
+              </div>
+
+              <p className={classes.text}>
+                Plot no.26, Cosmos Gardens,
+                <br />
+                Next to Learning Curve School,
+                <br />
+                Near Hotel Grillicious,
+                <br />
+                Baner, Pune - 411045
+              </p>
+            </div>
+          </div>
+
+          <div className={classes.mediaIcons}>
+            <a
+              href="https://in.linkedin.com/in/ca-chetan-gopalkar-62456066"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img alt="gopalkar-and-company-linkedin" src={Icon4} />
+            </a>
+
+            <a href="mailto: gopalkar.ca@gmail.com">
+              <img alt="gopalkar-and-company-email" src={Icon5} />
+            </a>
+          </div>
         </div>
 
-        <div className={classes.map}></div>
+        <div className={classes.map}>
+          <iframe
+            title="gopalkar-and-company-address"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.279526239389!2d73.78137176485787!3d18.561432537385176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf71c4ca6f29%3A0xdeeeda98108514be!2sGopalkar%20%26%20Company!5e0!3m2!1sen!2sin!4v1644489498352!5m2!1sen!2sin"
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            allow="fullscreen"
+            loading="lazy"
+          />
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
