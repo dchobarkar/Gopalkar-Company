f921ebc78c64e8556a34b1adb6ef76e5d6375535
Darshan Chobarkar <dchobarkar@gmail.com>
2022-02-10 20:20:29 +0530

    Not found page completed

--- src/components/NotFound/NotFound.css.js
+++ src/components/NotFound/NotFound.css.js
@@ -1,3 +1,47 @@
-const NotFoundCSS = { root: {} };
+import { Lib24, Link, Padding } from "../../css";
+
+const NotFoundCSS = {
+  root: {
+    minHeight: "85vh",
+    ...Padding,
+    paddingTop: "2rem",
+    paddingBottom: "2rem",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+    maxHeight: "400px",
+    maxWidth: "400px",
+  },
+  header: { ...Lib24 },
+  text: {
+    padding: "2rem 0",
+  },
+  link: { ...Link },
+
+  /* Tablet view */
+  "@media only screen and (min-width: 401px) and (max-width: 960px)": {
+    root: {
+      paddingBottom: "2rem",
+    },
+    text: {
+      width: "80%",
+    },
+  },
+
+  /* Desktop view */
+  "@media only screen and (min-width: 961px)": {
+    root: {
+      paddingBottom: "2rem",
+    },
+    text: {
+      width: "60%",
+    },
+  },
+};
 
 export default NotFoundCSS;
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
+import { Link } from "react-router-dom";
 
+import GIF from "../../assets/img/gopalkar-and-company-page-not-found.gif";
 import NotFoundCSS from "./NotFound.css";
 
 // CSS
@@ -10,7 +12,28 @@ const useStyles = makeStyles({ ...NotFoundCSS });
 function NotFound() {
   const classes = useStyles();
 
-  return <div className={classes.root}>Not Found Page</div>;
+  return (
+    <div className={classes.root}>
+      <img
+        className={classes.image}
+        alt="gopalkar-and-company-page-not-found"
+        src={GIF}
+      />
+
+      <h2 className={classes.header}>404 Page Not Found</h2>
+
+      <p className={classes.text}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum
+        ultrices nulla lorem bibendum eros tellus volutpat. Facilisi quis morbi
+        quam pulvinar lorem rhoncus. Euismod lectus aliquam massa tempor
+        fermentum. Tellus cursus natoque maecenas eget a ac facilisis.
+      </p>
+
+      <Link className={classes.link} to="/">
+        Home Page
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
